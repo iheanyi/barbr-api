5e517f4e6a6ec961df42a1d637a3cfc266f02949
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2016-05-22 17:41:34 -0400

    Add in user validation stuff.

--- web/models/user.ex
+++ web/models/user.ex
@@ -6,10 +6,13 @@ defmodule Barbr.User do
     field :username, :string
     field :password_hash, :string
 
+    # Virtual fields for password confirmation
+    field :password, virtual: true, :string
+    field :password_confirmation, virtual: true, :string
     timestamps
   end
 
-  @required_fields ~w(email username password_hash)
+  @required_fields ~w(email username password password_confirmation)
   @optional_fields ~w()
 
   @doc """
@@ -21,5 +24,18 @@ defmodule Barbr.User do
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:password, min: 8)
+    |> validate_confirmation(:password)
+    |> hash_password
+    |> unique_constraint(:email)
+    |> unique_constraint(:username)
+  end
+
+  defp hash_password(%{valid?: false} = changeset), do: changeset
+  defp hash_password(%{valid?: true} = changeset) do
+    hashedpw = Comeonin.BCrypt.hashpwsalt(Ecto.Changeset.get_field(changeset,
+    :password))
+    Ecto.Changeset.put_change(changeset, :password_hash, hashedpw)
   end
 end
